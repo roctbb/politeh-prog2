4b758a741ad88d41f4d1649d31fa1a36901a0422
Бородин Ростислав <roctbb@Rcw-Mac.local>
2015-02-20 16:13:28 +0300

    comments

--- SoundReverse/SoundReverse.cpp
+++ SoundReverse/SoundReverse.cpp
@@ -7,20 +7,11 @@
 int _tmain(int argc, _TCHAR* argv[])
 {
 	Wave audio;
-	Wave key;
-	audio.read("C:\\Prog\\politeh-prog2\\SoundReverse\\test.wav");
-	key.read("C:\\Prog\\politeh-prog2\\SoundReverse\\key.wav");
-	/*for (int i=0; i<audio.n/2; i++)
-	{
-		_int16 tmp = 10*audio.samples[i];
-		audio.samples[i] = 120*audio.samples[audio.n-i-1];
-		audio.samples[audio.n-i-1]=tmp;
-	}*/
-	for (int i=0; i<audio.n; i++)
-	{
-		audio.samples[i]^=key.samples[i];
-	}
-	audio.write("C:\\Prog\\politeh-prog2\\SoundReverse\\test1.wav");
+	
+	audio.read("Z:\\politeh-prog2\\SoundReverse\\task1.wav");
+	//audio.samples - массив сэмплов
+	//audio.n - размер этого массива
+	audio.write("Z:\\politeh-prog2\\SoundReverse\\answer2.wav");
 	
 	return 0;
 }
